2f44e7c00913dc483d441501ea68255cf468b231
Renan Vieira <renanleitev@gmail.com>
2023-05-08 10:08:01 -0300

    first commit

---
+++ src/main/java/io/bootify/my_app/service/UserService.java
@@ -0,0 +1,60 @@
+package io.bootify.my_app.service;
+
+import io.bootify.my_app.domain.User;
+import io.bootify.my_app.model.UserDTO;
+import io.bootify.my_app.repos.UserRepository;
+import io.bootify.my_app.util.NotFoundException;
+import java.util.List;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(final UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<UserDTO> findAll() {
+        final List<User> users = userRepository.findAll(Sort.by("id"));
+        return users.stream()
+                .map((user) -> mapToDTO(user, new UserDTO()))
+                .toList();
+    }
+
+    public UserDTO get(final Long id) {
+        return userRepository.findById(id)
+                .map(user -> mapToDTO(user, new UserDTO()))
+                .orElseThrow(NotFoundException::new);
+    }
+
+    public Long create(final UserDTO userDTO) {
+        final User user = new User();
+        mapToEntity(userDTO, user);
+        return userRepository.save(user).getId();
+    }
+
+    public void update(final Long id, final UserDTO userDTO) {
+        final User user = userRepository.findById(id)
+                .orElseThrow(NotFoundException::new);
+        mapToEntity(userDTO, user);
+        userRepository.save(user);
+    }
+
+    public void delete(final Long id) {
+        userRepository.deleteById(id);
+    }
+
+    private UserDTO mapToDTO(final User user, final UserDTO userDTO) {
+        userDTO.setId(user.getId());
+        return userDTO;
+    }
+
+    private User mapToEntity(final UserDTO userDTO, final User user) {
+        return user;
+    }
+
+}
